befe4e8016cebe400b780f3bf7e113fa65332f04
xbx0119 <huangyifan0119@163.com>
2018-05-05 21:39:56 +0800

    genesisblock

--- bin/setup.sh
+++ bin/setup.sh
@@ -3,15 +3,23 @@
 # 设置当前工作目录为bin
 cd "$(dirname "$0")"
 
-userConfig="../config/config.user.js"
+file_userConfig="../config/config.user.js"
+file_genesisBlock="../genesisBlock.json"
 
-
+genesisBlock=`cat $file_genesisBlock`
 
 # 创建初始化用户配置
-if [ ! -f "$userConfig" ]; then
-    echo 'const config = {\n\n}; \n\nexport default config;' > $userConfig
+if [ ! -f "$file_userConfig" ]; then
+    echo 'const config = {\n\n}; \n\nexport default config;' > $file_userConfig
 
     echo "创建config.user.js"
+fi
+
+# 数据库插入创世区块
+blocks=`mongo chain -u root -p 806119 --authenticationDatabase "admin" --eval "db.blocks.find({})" --quiet`
+if [ "$blocks" = "" ]; then
+    mongo chain -u root -p 806119 --authenticationDatabase "admin" --eval "db.blocks.insert($genesisBlock)" --quiet
+    echo "创世区块创建成功"
 fi
 
 
